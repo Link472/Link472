6cde1ea28e01a62dbd956092b3ce88ce2d89b3d8
Patrick Miller <64869968+Link472@users.noreply.github.com>
2022-04-25 16:18:14 -0500

    Add files via upload

---
+++ Mod12Homework.ps1
@@ -0,0 +1,18 @@
+﻿# Patrick Miller
+# Mod 12 Homework 
+
+# Declerations 
+
+# Make a path to the current desktop
+$user_path = "C:\Users\" + $env:USERNAME + "\Desktop"
+Set-Location $user_path
+
+# Write out headers
+Write-Output ("Date,Time,Priority,Classification,Description,Scource IP, Destination IP") | Out-File "alert_data.csv" -encoding ascii
+
+# Read line by line 
+
+# $dog = $line.split(“:”, [System.StringSplitOptions]::RemoveEmptyEntries)
+
+# End script 
+Read-Host "Processing is done. Press enter to close the script"
