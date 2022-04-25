d50de7ff5d7c7ad0720370856cc95faf07708d17
Patrick Miller <64869968+Link472@users.noreply.github.com>
2022-04-25 16:24:59 -0500

    Add files via upload

--- Mod12Homework.ps1
+++ Mod12Homework.ps1
@@ -4,13 +4,27 @@
 # Declerations 
 
 # Make a path to the current desktop
-$user_path = "C:\Users\" + $env:USERNAME + "\Desktop"
-Set-Location $user_path
+$desktop_path = "C:\Users\" + $env:USERNAME + "\Desktop"
+Set-Location $desktop_path
 
 # Write out headers
-Write-Output ("Date,Time,Priority,Classification,Description,Scource IP, Destination IP") | Out-File "alert_data.csv" -encoding ascii
+Write-Output ("Date,Time,Priority,Classification,Description,Scource IP,Destination IP") | Out-File "alert_data.csv" -encoding ascii
 
 # Read line by line 
+$target_file = ".\fast_short.pcap"
+$data = @()
+
+foreach ($line in Get-Content $target_file)
+{
+    if ($line.ReadCount -eq 1)
+    {
+       continue 
+    }
+
+    $split1 = $line.split(",")
+    $city = $split1[2].trim(" ")
+    $employee_by_city += ,@($city + ": ")
+}
 
 # $dog = $line.split(“:”, [System.StringSplitOptions]::RemoveEmptyEntries)
 
