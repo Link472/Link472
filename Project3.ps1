e54bc6324c54d5ab7cf354ded958d6ac4738d100
Patrick Miller <64869968+Link472@users.noreply.github.com>
2022-04-27 16:38:06 -0500

    Add files via upload

---
+++ Project3.ps1
@@ -0,0 +1,28 @@
+﻿# Patrick Miller
+# Project 3
+
+# Declarations 
+
+# Move to home directory 
+
+# Set path to user desktop
+$desktop_path = "C:\Users\" + $env:USERNAME + "\Desktop"
+Set-Location $desktop_path
+
+# Functions 
+
+# Startup Function 
+If ( -not (Test-Path “path you are testing”)) 
+{
+    
+}
+
+# Process Mixed URLs 
+
+# Process Bad URLs
+
+# Search titles by partial lookup
+
+# Call to startup function 
+
+# Menu 
